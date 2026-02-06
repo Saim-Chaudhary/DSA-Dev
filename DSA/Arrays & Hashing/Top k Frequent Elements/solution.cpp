f342003c164a19e88d07aff1640c6e491ff5859d
Saim Ch <saimch726@gmail.com>
2026-02-07 00:31:08 +0500

    Add Top k Frequent Element Solution

--- DSA/Arrays & Hashing/Top k Frequent Elements/solution.cpp
+++ DSA/Arrays & Hashing/Top k Frequent Elements/solution.cpp
@@ -1,43 +1,44 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main() {
-    int nums[] = {1, 3, 4, 3, 4, 2, 3, 4, 2, 3, 4, 2, 5, 4, 5, 5};
-    int size = sizeof(nums) / sizeof(nums[0]);
-    int k = 3;
+class Solution
+{
+public:
+    vector<int> topKFrequent(vector<int> &nums, int k)
+    {
+        //declaring hashmap to store element frequencies
+        unordered_map<int, int> mp;
+        
+        //counting frequency of each element
+        for (int x : nums)
+        {
+            mp[x]++;
+        }
 
-    //declaring hashmap
-    map<int, int> mp;
-    
-    //inserting values in hashmap
-    for(int i = 0; i < size; i++){
-        mp[nums[i]]++;
-    }
+        // priority_queue<Type, Container, Comparison>...(min-heap)
+        //using pair to store {frequency, element} from hashmap
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
-    // priority_queue<Type, Container, Comparison>...(min-heap)
-    //use pair becuase hashmap contains two things [elemets, frequency]
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    for(auto n : mp){
-        pq.push({n.second, n.first});
+        for (auto n : mp)
+        {
+            //push {frequency, element} in min-heap
+            pq.push({n.second, n.first});
 
-        if(pq.size() > k){
+            //if size exceeds k, remove smallest frequency element
+            if (pq.size() > k)
+            {
+                pq.pop();
+            }
+        }
+
+        //storing final size of priority queue
+        int actual_size = pq.size();
+        vector<int> ans(actual_size);
+
+        //extracting elements from priority queue
+        for (int i = 0; i < actual_size; i++)
+        {
+            ans[i] = pq.top().second; //.second because in priority_queue second is our actual element
             pq.pop();
         }
+
+        return ans;
     }
-
-    int priority_queue_size = pq.size();
-    int ans[priority_queue_size];
-
-    for(int i = 0; i < priority_queue_size; i++){
-
-        //putting top elements in array
-        ans[i] = pq.top().second; //.second because in priority_queue second is our actual value
-        
-        //move to next element
-        pq.pop();
-    }
-
-    for(int i = 0; i < priority_queue_size; i++){
-        cout<<ans[i]<<endl;
-    }
-    
-}
+};
